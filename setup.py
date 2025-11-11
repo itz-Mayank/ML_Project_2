00db6acac4cb80772b06f3d4b65b6bbb2ce37058
Mayank Meghwal <mayankmeg207@gmail.com>
2025-11-11 18:01:13 +0530

    added setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,32 @@
 # Here i will generate my code for generating the .egg file for logs
-from google import genai
+'''
+This setup.py file really important because for any project that you probably develop you really need to have this setup.py because it is an essential part of packaging and distributing python projects. it is used by setup tools to define the configuration of a project such as this metadata, dependency
+and more.
+'''
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    try:
+        with open(file_path) as file_obj:
+            requirements=file_obj.readlines()
+            requirements=[req.replace("\n","") for req in requirements]
+            
+            if "-e ." in requirements:
+                requirements.remove("-e .")
+    except FileNotFoundError:
+        print("requirements.txt file not found.")
+    return requirements
 
-# The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client()
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+setup(
+    name="network_security_project",
+    version="0.0.1",
+    author="Mayank Meghwal",
+    author_email="mayankmeg207@gmail.com",
+    description="A project for network security using machine learning",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
 )
-print(response.text)
