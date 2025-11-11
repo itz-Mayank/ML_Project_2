91efb032e43e2b84618188ac68c5e2e24cbe9cc5
Mayank Meghwal <mayankmeg207@gmail.com>
2025-11-11 17:20:32 +0530

    added after pull

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
 # Here i will generate my code for generating the .egg file for logs
+from google import genai
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+)
+print(response.text)
