cf4dbf012839b3a318c757fa464f474fa20e0356
itz-Mayank <mayankmeg207@gmail.com>
2025-12-21 23:09:11 +0530

    transformation

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -35,3 +35,18 @@ DATA_VALIDATION_VALID_DIR = "validated"
 DATA_VALIDATION_INVALID_DIR = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME = "report.yaml"
+
+
+
+'''
+Data transformation related constant start with DATA_TRANSFORMATION variable name
+'''
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_OBJECT_DIR: str = "transformed_object"
+
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values" : np.nan,
+    "n_neighbors":3,
+    "weights": "uniform"
+}
